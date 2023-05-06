0e1bbe63151c705b6fa1436199151fa4383a9764
Douglas81 <douglas81silveira@gmail.com>
2023-05-06 14:22:07 -0300

    Implementação da api e dos componentes

---
+++ src/components/Input/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  border: 1px solid #fafafa;
+  border-radius: 20px;
+  width: 80%;
+  height: 62px;
+  margin: 20px;
+
+  input {
+    background: transparent;
+    border: 0;
+    width: 100%;
+    height: 62px;
+    padding: 0 20px;
+    color: #ffffff;
+    font-size: 20px;
+  }
+`;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,5 +1,52 @@
+import { useState } from "react";
+import githubLogo from "../assets/github.png";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import ItemRepo from "../components/ItemRepo";
+import { api } from "../services/api";
+
+import { Container } from "./styles";
+
 function App() {
-  return <div className="App">Olá</div>;
+  const [currentRepo, setCurrentRepo] = useState("");
+  const [repos, setRepos] = useState([]);
+
+  const handleSearchRepo = async () => {
+    await api
+      .get(`repos/${currentRepo}`)
+      .then((value) => value.data)
+      .then((data) => {
+        const isExists = repos.find((repo) => repo.id === data.id);
+        if (!isExists) {
+          setRepos((prev) => [...prev, data]);
+        } else {
+          alert("Repositório já adicionado");
+        }
+        setCurrentRepo("");
+      })
+      .catch((e) => {
+        alert(`Erro ao buscar repositório: ${e}`);
+        setCurrentRepo("");
+      });
+  };
+
+  const handleRemoveRepo = (id) => {
+    setRepos(repos.filter((repo) => repo.id !== id));
+  };
+
+  return (
+    <Container>
+      <img src={githubLogo} width={72} height={72} alt="github" />
+      <Input
+        value={currentRepo}
+        onChange={(e) => setCurrentRepo(e.target.value)}
+      />
+      <Button onClick={handleSearchRepo} />
+      {repos.map((repo) => (
+        <ItemRepo repo={repo} handleRemoveRepo={handleRemoveRepo} />
+      ))}
+    </Container>
+  );
 }
 
 export default App;
